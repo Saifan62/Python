b52c172940c647e96766c325b0243f353e5ab81c
Saifan62 <saifanzaman681@gmail.com>
2025-07-19 11:11:09 +0600

    Add line counting functionality and update content in Saifan's lifestyle.txt

--- File Handling- access modes.py
+++ File Handling- access modes.py
@@ -1,20 +1,11 @@
-file = open("Saifan's lifestyle.txt", 'r')
-Counter = 0
-Content= file.read()
-Colist = Content.split('\n')
-for i in Colist:
-    if i:
-        Counter += 1
-print("This is the number of lines in the file:")
-print(Counter)
-file.close()
+
 
 file_write = open("Saifan's lifestyle.txt", 'w')
 print("File opened in write mode:")
 file_write.write("Saifan is my best friend. His Minecraft name is SaifanBrine.")
 file_write.close()
 
-file_append = open("Saifan's lifestyle.txt", 'w')
+file_append = open("Saifan's lifestyle.txt", 'a')
 print("File opened in write mode:")
 file_append.write("And me, I am his best friend my name is Arhaan. I am a c++ developer.My minecraft name is ZephyrV2.") 
 
